bc62afeabad5200f09f5166d8229f5cb320be44a
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-15 19:45:38 +0100

    added return content for Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
+  const renderedItems = items.map(item => {
+    return (
+      <div>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    )
+  })
+
   return <h1>{items.length}</h1>
 };
 
