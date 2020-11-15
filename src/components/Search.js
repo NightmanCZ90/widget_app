f8276a808b75cd0730e1eaa3ae7364314ffd0078
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-16 00:26:49 +0100

    added a link to searched articles

--- src/components/Search.js
+++ src/components/Search.js
@@ -28,6 +28,15 @@ const Search = () => {
   const renderedResults = results.map(result => {
     return (
       <div key={result.pageid} className="item" >
+        <div className="right floated content">
+          <a 
+            target="_blank"
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">
             {result.title}
