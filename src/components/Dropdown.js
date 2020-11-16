225c1a31189eb9037ad38c00567c394278095082
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-16 19:08:57 +0100

    added jsx structure

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,10 +6,21 @@ const Dropdown = ({ options }) => {
       <div key={option.value} className="item">
         {option.label}
       </div>
-    )
-  })
+    );
+  });
 
-  return renderedOptions;
+  return (
+    <div className="ui form" >
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select Color</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
