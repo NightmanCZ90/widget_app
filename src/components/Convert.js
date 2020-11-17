9e25aa3dccb9a255d3b4c6bcf0d84a7443c39ae4
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-17 21:22:16 +0100

    working axios request

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import googleTranslateAPI from '../api/googleAPI';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
   useEffect(() => {
-    console.log('New Language or text')
+    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+      params: {
+        q: text,
+        target: language.value,
+        key: googleTranslateAPI
+      }
+    })
   }, [language, text]);
 
   return (
